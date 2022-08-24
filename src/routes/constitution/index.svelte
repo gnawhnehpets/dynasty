c228e07a0419ad4c1b8718b9c4211ae3c709c68a
stephenhwang <steveyhwang@gmail.com>
2022-08-23 20:12:36 -0400

    constitution commit 2

--- src/routes/constitution/index.svelte
+++ src/routes/constitution/index.svelte
@@ -2,4 +2,4 @@
 	import PdfViewer from 'svelte-pdf';
 </script>
 
-<PdfViewer url='https://raw.githubusercontent.com/vinodnimbalkar/svelte-pdf/369db2f9edbf5ab8c87184193e1404340729bb3a/public/sample.pdf' />
+<PdfViewer url='./constitution.pdf' />
