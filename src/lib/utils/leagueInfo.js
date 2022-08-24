8f95bddf6ecf312c883a63a32e72fba113c669d4
stephenhwang <steveyhwang@gmail.com>
2022-08-23 21:13:03 -0400

    league set 3

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -8,7 +8,18 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 /*   STEP 2   */
 export const homepageText = `
   <p>Welcome to the inaugural salary cap dynasty league!</p>
-  <p>Please review the rules <a href="https://dynasty-git-master-stephenyounghwang-gmailcom.vercel.app/constitution">here</a>.</p>
+  <p>No consensus on date so far, but the most amenable seems to be 8/28 on Sunday 7-10 PM. If this date is completely not doable please reach out to me ASAP.  I know this is short notice, but hopefully we can all carve out time on this date.</p><p>League Rules Version 1.0 (Subject to change, but major cornerstones of what should be good are in place)
+  <a href="https://dynasty-git-master-stephenyounghwang-gmailcom.vercel.app/constitution">here</a>.</p>
+  <p>Please send me dues as soon as possible to confirm your spot. Once everything is in place, I will randomly draw the rookie draft seeding.</p><p>Also, this is a major change, but we are going to try a new fantasy app out Sleeper Invite to the league: https://sleeper.app/i/jEGwPG2lXxXw</p>
+  <p>Development was geared towards the mobile app. You can use https://sleeper.com/login and try the web version.</p>
+  <p>Venmo: @Scott-Hur</br>Quickpay: shur86@gmail.com</p>
+  
+  <p>Start researching your rookies!!!!!</p>
+  
+  <p>Want to reiterate that there may be some growing pains for the first year, but I will try my best to make this an enjoyable league for all participants and iterate towards maintaining a worthwhile and compelling long term league</p>
+  
+  <p>Thanks,</br>
+  Scott</p>
 `;
 
 /*   STEP 3   */
